b4174e6b8bbe1f65dbc9e4227eb39bf4271c2bde
barakavraham2 <barak.avraam@gmail.com>
2021-07-23 18:02:45 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
+import React from 'react';
 import { Container, Content, FlexboxGrid } from 'rsuite';
 import DrawerB from './Drawer';
 import ContectUs from './comp/ContectUs';
 import Home from './comp/Home';
 import About from './comp/About';
 import { Switch, Route } from 'react-router-dom'
+import AppFooter from './views/AppFooter';
+import Scroll from 'react-scroll';
+
 function App() {
+
+
   return (
-    <Container className="container">
-      <DrawerB></DrawerB>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/contact">
-          <ContectUs />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-      </Switch>
-    </Container>
+    <div className="page">
+      <Container className="container" style={{ height: '100%' }}>
+        <DrawerB></DrawerB>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/contact">
+            <ContectUs />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Container>
+      <AppFooter />
+    </div>
   );
 }
 
