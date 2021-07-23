490de73dec9460846e7edd613d3c8b9c39c74247
barakavraham2 <barak.avraam@gmail.com>
2021-07-24 02:07:27 +0300

    about

--- src/comp/About.js
+++ src/comp/About.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Content } from 'rsuite';
+import ico from '../img/favicon.png'
+import Fade from 'react-reveal/Fade';
 
 function About() {
     return (
-        <div>
-
-        </div>
+        <Content className="About">
+            <div className="logo">
+                <img src={ico} className="logo-item" />
+            </div>
+            <Fade up>
+                <div className="AboutHead">
+                    <p>About Teezer</p>
+                    <div className="aboutCon">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        </p>
+                    </div>
+                </div>
+            </Fade>
+        </Content>
     );
 }
 
