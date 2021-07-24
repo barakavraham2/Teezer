3366c0eb6401910a93008ab17ad954faa38cb003
barakavraham2 <barak.avraam@gmail.com>
2021-07-24 18:52:14 +0300

    test

--- src/comp/ContectUs.js
+++ src/comp/ContectUs.js
@@ -15,28 +15,17 @@ import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import Fade from 'react-reveal/Fade';
 
 const useStyles = makeStyles((theme) => ({
-    appBar: {
-        position: 'relative',
-    },
     layout: {
         width: 'auto',
         marginLeft: theme.spacing(2),
         marginRight: theme.spacing(2),
-        [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-            width: 600,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
+        marginLeft: 'auto',
+        marginRight: 'auto',
     },
     paper: {
         marginTop: theme.spacing(3),
         marginBottom: theme.spacing(3),
-        padding: theme.spacing(2),
-        [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-            marginTop: theme.spacing(6),
-            marginBottom: theme.spacing(6),
-            padding: theme.spacing(3),
-        },
+        padding: theme.spacing(2)
     },
     stepper: {
         padding: theme.spacing(3, 0, 5),
